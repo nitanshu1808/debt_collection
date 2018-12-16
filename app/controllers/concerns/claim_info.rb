466516eaa236793c08361fdf02835bed91d16d84
Nitanshu Rehani <nitanshu.nik@gmail.com>
2018-12-16 13:38:21 +0000

    - File Preview

--- app/controllers/concerns/claim_info.rb
+++ app/controllers/concerns/claim_info.rb
@@ -3,6 +3,7 @@ module ClaimInfo
 
   def show
     @claim  = Claim.find_by(id: params["id"])
+    @doc    = @claim.document
     @debtor = @claim.debtor
     @owner  = @claim.business
     render 'shared/claim'
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  VALID_FILE_TYPE     = ["image/gif", "image/jpeg", "image/png"];
 end
