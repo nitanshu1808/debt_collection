42b0859ce61ab714ec57898d0e080529aed58f8b
Nitanshu Rehani <nitanshu.nik@gmail.com>
2018-12-20 21:32:54 +0000

    - commented seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-lawyer   = FactoryBot.create_list(:lawyer, 20)
-business = FactoryBot.create_list(:business, 20)
+# lawyer   = FactoryBot.create_list(:lawyer, 20)
+# business = FactoryBot.create_list(:business, 20)
 
-CollectionArea.create(CollectionArea::TYPES.map{|collection_area| {name: collection_area} })
+# CollectionArea.create(CollectionArea::TYPES.map{|collection_area| {name: collection_area} })
